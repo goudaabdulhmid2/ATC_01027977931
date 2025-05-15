1fe8cfbad87c8f89b3295da1d283f07d06b94b02
Abdulhamid Gouda <goudaabdulhmid@gmail.com>
2025-05-16 00:52:05 +0300

    refactor(SendEmail): simplify password reset link generation
    The password reset link generation in `sendPasswordReset` was simplified by directly embedding the code in the message instead of constructing a full URL. This reduces unnecessary string concatenation and improves code readability.

--- server/utils/SendEmail.js
+++ server/utils/SendEmail.js
@@ -38,23 +38,17 @@ module.exports = class Email {
 
   // Password reset email
   async sendPasswordReset(protocol, host) {
-    const resetLink = `${protocol}://${host}/reset-password?code=${this.code}`;
-    
     const message = `Hello ${this.firstname},
 
         We received a request to reset your password on your account.
 
         Please click on the following link to reset your password (valid for 10 minutes):
 
-        ${resetLink}
+        ${this.code}
 
-        If you did not request a password reset, please ignore this email.`
-;
-  
-    await this.send(
-      message,
-      'Your password reset link (valid for 10 minutes)'
-    );
+
+        If you did not request a password reset, please ignore this email.`;
+    await this.send(message, 'Your password reset link (valid for 10 minutes)');
   }
 
   async sendWelcome() {
